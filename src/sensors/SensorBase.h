0cf19212644a57d023c296f792edc762c78a1112
Hubert2208 <46359915+Hubert2208@users.noreply.github.com>
2026-07-03 17:41:41 +0200

    fix(sensor): each sensor uses its own updateIntervalMs

--- src/sensors/SensorBase.h
+++ src/sensors/SensorBase.h
@@ -30,6 +30,9 @@ public:
     bool isEnabled() const { return _enabled; }
     void setEnabled(bool e) { _enabled = e; }
 
+    void setUpdateIntervalMs(int ms) { _updateIntervalMs = ms; }
+    int updateIntervalMs() const { return _updateIntervalMs; }
+
     unsigned long lastReadTime() const { return _lastRead; }
 
 protected:
@@ -37,6 +40,7 @@ protected:
     bool _connected = false;
     float _value = 0.0f;
     unsigned long _lastRead = 0;
+    int _updateIntervalMs = 2000;
 };
 
 #endif // SENSOR_BASE_H
